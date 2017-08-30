3096b169847f77a5ae1e28e0b2659d5a831c0e1c
Todd Moore <todd@toddm.me>
2017-08-30 11:31:16 +1000

    NODE_ENV test

--- incorrectStyleGuidePath.js
+++ incorrectStyleGuidePath.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
+const notTesting = process.env.NODE_ENV !== 'test';
+
 const explicitlyResolvedIncorrectPath = path.resolve(__dirname, 'node_modules/seek-style-guide');
 const resolvedPath = require.resolve('seek-style-guide');
 
-module.exports = resolvedPath.indexOf(explicitlyResolvedIncorrectPath) > -1;
+module.exports = notTesting && resolvedPath.indexOf(explicitlyResolvedIncorrectPath) > -1;
