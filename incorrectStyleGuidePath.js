6358c110b3887675a95dff935abecdea5ff0f529
Todd Moore <tamoore@users.noreply.github.com>
2017-09-01 10:37:23 +1000

    refactor(Debugging): Errors when module is linked locally (#12)

---
+++ incorrectStyleGuidePath.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+const notTesting = process.env.NODE_ENV !== 'test';
+
+const explicitlyResolvedIncorrectPath = path.resolve(__dirname, 'node_modules/seek-style-guide');
+const resolvedPath = require.resolve('seek-style-guide');
+
+module.exports = notTesting && resolvedPath.indexOf(explicitlyResolvedIncorrectPath) > -1;
