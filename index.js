3dc702874436106d6777cb41e954c5c555da631d
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-10-09 20:26:57 +1100

    fix(React): Add displayName to React components

--- index.js
+++ index.js
@@ -95,7 +95,8 @@ const getCommonLoaders = (includes) => [
           ],
           plugins: [
             require.resolve('babel-plugin-transform-class-properties'),
-            require.resolve('babel-plugin-transform-object-rest-spread')
+            require.resolve('babel-plugin-transform-object-rest-spread'),
+            require.resolve('babel-plugin-add-react-displayname')
           ]
         }
       }
