b0456e38d05b9fbc93e11cf6ffaaf4cc79cbeab4
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-10-11 10:15:40 +1100

    fix(less): Add missing Less dependency (#15)
    When the webpack 1 decorators were removed from seek-style-guide, so too was the style guide's Less dependency. This package didn't include Less, which meant it failed to build on the latest major style guide version.

--- autoprefixer.config.js
+++ autoprefixer.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-   browsers: [
-     '> 1%',
-     'Last 2 versions',
-     'IE >= 10',
-     'Edge >= 13',
-     'Safari >= 6',
-     'iOS >= 6',
-     'Samsung >= 4'
-   ]
+  browsers: [
+    '> 1%',
+    'Last 2 versions',
+    'IE >= 10',
+    'Edge >= 13',
+    'Safari >= 6',
+    'iOS >= 6',
+    'Samsung >= 4'
+  ]
 };
